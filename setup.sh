077be1aedbc7684cd440281ee1d89156653fee41
langdonholmes <55119338+langdonholmes@users.noreply.github.com>
2021-03-15 14:24:40 -0700

    Heroku not cooperating

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
-mkdir -p ~/.streamlitecho "[server]
-headless = true
-port = $PORT
-enableCORS = false
+mkdir -p ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
